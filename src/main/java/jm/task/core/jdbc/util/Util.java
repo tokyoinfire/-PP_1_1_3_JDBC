fb7895c3870027a1c20b8f5bd515b87bb49370d0
tokyoinfire <49620904+tokyoinfire@users.noreply.github.com>
2022-08-23 14:33:00 +0300

    Add files via upload
    initial commit

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection connectToDB() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения!");
+        }
+        return connection;
+    }
+
+
+
+
+}
